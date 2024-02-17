9cea710d52708d35b6e0e99848f7672750275ad5
Animesh Kumar <kumaranimesh097@gmail.com>
2024-02-17 12:32:00 +0530

    remove User & useCounter

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,8 @@
-import User from "./User";
-
 const About = () => {
   return (
     <div>
       <h2>About Page</h2>
-      <User name={"abc"} location={"XYZ"} />
+      {/* <User name={"abc"} location={"XYZ"} /> */}
     </div>
   );
 };
